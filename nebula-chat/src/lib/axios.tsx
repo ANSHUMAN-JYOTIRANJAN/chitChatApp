9ebf9927472a1224e0c16ca0036a9317bd167653
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-02-05 19:47:50 +0530

    dist added

--- nebula-chat/src/lib/axios.tsx
+++ nebula-chat/src/lib/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5000" 
-  : "https://nebula-server-dps6.onrender.com";
+  : "https://chitchatapp-1d07.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
